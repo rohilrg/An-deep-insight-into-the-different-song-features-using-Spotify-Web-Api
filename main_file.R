1d669000b65fba8fa18e820f5dd0cfbfd88f4f3b
Rohil Gupta <rohil.rg@gmail.com>
2018-02-22 14:08:51 +0100

    V1

--- main_file.R
+++ main_file.R
@@ -1,1 +1,11 @@
+md <- combined_dataset
+cor_valence <- cbind(combined_dataset$loudness, combined_dataset$energy, combined_dataset$tempo, combined_dataset$liveness,combined_dataset$danceability, combined_dataset$speechiness, combined_dataset$instrumentalness,combined_dataset$acousticness, combined_dataset$track_popularity, combined_dataset$album_popularity)
+colnames(cor_valence)<- c("Loudness", "Energy", "Tempo","Liveness", "Danceability", "Speechiness","Instrumentalness", "Accousticness", "Track Popularity", "Album Popularity")
+m<- cor(combined_dataset$valence, cor_valence)
+m
+
+barplot(m, axes = TRUE, beside = TRUE)
+
+#According to the correlation plot shows Danceablity> Energy> Loudness> Tempo> Speechiness with Valence.
+
 
