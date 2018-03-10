a13cf85abf7ebecc3b28f4d4977e2cb5171e436d
Rohil Gupta <rohil.rg@gmail.com>
2018-03-10 16:54:09 +0100

    Updated

--- main_file.R
+++ main_file.R
@@ -1,20 +1,61 @@
-md <- combined_dataset
-cor_valence <- cbind(combined_dataset$loudness, combined_dataset$energy, combined_dataset$tempo, combined_dataset$liveness,combined_dataset$danceability, combined_dataset$speechiness, combined_dataset$instrumentalness,combined_dataset$acousticness, combined_dataset$track_popularity, combined_dataset$album_popularity)
-colnames(cor_valence)<- c("Loudness", "Energy", "Tempo","Liveness", "Danceability", "Speechiness","Instrumentalness", "Accousticness", "Track Popularity", "Album Popularity")
-m<- cor(combined_dataset$valence, cor_valence)
-m
+# ensure the results are repeatable
+set.seed(7)
+# load the library
+library(mlbench)
+library(caret)
+combined_dataset <- read.csv('C:/Users/Rohil/Documents/Music_Dataset.csv')
+combined_dataset$valence_b[combined_dataset$valence>=0.50]<- "1"
+combined_dataset$valence_b[combined_dataset$valence<0.50]<- "0"
+combined_dataset$valence_b <- factor(combined_dataset$valence_b)
+combined_dataset$mode_b[combined_dataset$mode=='major']<- "1"
+combined_dataset$mode_b[combined_dataset$mode=='minor']<- "0"
+combined_dataset$mode_b <- factor(combined_dataset$mode_b)
 
-barplot(m, axes = TRUE, beside = TRUE)
+usefull_features <- cbind(combined_dataset$loudness, combined_dataset$energy, combined_dataset$tempo, combined_dataset$liveness,combined_dataset$danceability, combined_dataset$speechiness, combined_dataset$instrumentalness,combined_dataset$acousticness, combined_dataset$track_popularity, combined_dataset$album_popularity, combined_dataset$mode_b)
+colnames(usefull_features)<- c("Loudness", "Energy", "Tempo","Liveness", "Danceability", "Speechiness","Instrumentalness", "Accousticness", "Track_Popularity", "Album Popularity", 'Mode')
 
-#According to the correlation plot shows Danceablity> Energy> Loudness> Tempo> Speechiness
-#with Valence.So these attributes can be used to predict the right music for a given track with most coherent valence with song.
 
-most_cor_attributes <- cbind(combined_dataset$artist_name,combined_dataset$track_name,combined_dataset$loudness, combined_dataset$energy, combined_dataset$tempo, combined_dataset$danceability, combined_dataset$speechiness, combined_dataset$valence)
-colnames(most_cor_attributes) <- c("Artist Name","Track Name","Loudness", "Energy", "Tempo", "Danceability", "Speechiness", "Valence")
 
-library(h2o)
-h2o.init()
-split_data <- h2o.splitFrame(data=most_cor_attributes, ratios = 0.75)
+##----------------------Removing Redundant Features##############_________
 
-training_data <- split_data[[1]]
-test_data <- split_data[[2]]
+# calculate correlation matrix
+correlationMatrix <- cor(usefull_features)
+# summarize the correlation matrix
+print(correlationMatrix)
+# find attributes that are highly corrected (ideally >0.75)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.75)
+# print indexes of highly correlated attributes
+print(highlyCorrelated)
+usefull_features_after_process <- cbind(combined_dataset$loudness, combined_dataset$energy, combined_dataset$tempo, combined_dataset$liveness,combined_dataset$danceability, combined_dataset$speechiness, combined_dataset$instrumentalness,combined_dataset$acousticness, combined_dataset$album_popularity, combined_dataset$mode_b)
+colnames(usefull_features_after_process)<- c("Loudness", "Energy", "Tempo","Liveness", "Danceability", "Speechiness","Instrumentalness", "Accousticness", "Album Popularity", 'Mode')
+#######_______ According to above method Track Popularity is highly correlated,So
+## it is redundant variable and it is removed from the dataset.
+
+
+
+### Now to find the importance of the features in relation to  the valence as the 
+#target variable, we use a ROC curve analysis conducted for each attribute.
+
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+# train the model
+model <- train(usefull_features_after_process, y=combined_dataset$valence_b, method="lvq", preProcess="scale", trControl=control)
+# estimate variable importance
+importance <- varImp(model, scale=FALSE)
+# summarize importance
+print(importance)
+# plot importance
+plot(importance)
+
+
+#### I also used the random forest technique to  find the importance of the each feature.
+
+# define the control using a random forest selection function
+control <- rfeControl(functions=rfFuncs, method="cv", number=10)
+# run the RFE algorithm
+results <- rfe(usefull_features_after_process, combined_dataset$valence_b, sizes=c(1:10), rfeControl=control)
+# summarize the results
+print(results)
+# list the chosen features
+predictors(results)
+# plot the results
+plot(results, type=c("g", "o"))
